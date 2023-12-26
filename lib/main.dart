934d479ab3f751e8ade53eb2ab7ec79086404343
Kevin <47770053+kevinwchen@users.noreply.github.com>
2023-12-26 23:53:14 +1100

    Add files

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Flutter Demo', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  final _notesStream =
+      Supabase.instance.client.from('notes').stream(primaryKey: ['id']);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Notes')),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _notesStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final notes = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(notes[index]['body']),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: ((context) {
+              return SimpleDialog(
+                title: const Text('Add a Note'),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                children: [
+                  TextFormField(
+                    onFieldSubmitted: (value) async {
+                      await Supabase.instance.client
+                          .from('notes')
+                          .insert({'body': value});
+                      if (!context.mounted) return;
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              );
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
